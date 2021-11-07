2e8f1d6864c951014952b1ee5ac83f99995a90be
Roxana <nolvia.roxana.aguilera@gmail.com>
2021-11-07 15:40:39 +0100

    Añade clase Cambio2

---
+++ IconosSuperpuestos/src/Cambio1/Cambio2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Cambio1;
+
+/**
+ * @author nolvi
+ *
+ */
+public class Cambio2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	 System.out.println("Clase Cambio2 en proyecto1"); 
+	 System.out.println("Clase Cambio2 en proyecto1"); 
+
+	}
+
+}
